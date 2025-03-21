75eba15e050ad35e1794d22f17049867ce4f25d2
LuisTernaNunes <luisterna@gmail.com>
2025-03-21 15:23:24 -0300

    Classe cliente

--- src/main/java/med/voll/api/clientes/Cliente.java
+++ src/main/java/med/voll/api/clientes/Cliente.java
@@ -1,4 +1,11 @@
 package med.voll.api.clientes;
 
+import med.voll.api.endereco.Endereco;
+
 public class Cliente {
+    private Long id;
+    private String nome;
+    private String telefone;
+    private String email;
+    private Endereco endereco;
 }
