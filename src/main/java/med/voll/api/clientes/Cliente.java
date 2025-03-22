3b0ebc6b3db35fb78c8085a8f84e11bd0b7c440e
LuisTernaNunes <luisterna@gmail.com>
2025-03-22 15:36:28 -0300

    Add arquivos cliente

--- src/main/java/med/voll/api/clientes/Cliente.java
+++ src/main/java/med/voll/api/clientes/Cliente.java
@@ -1,11 +1,27 @@
 package med.voll.api.clientes;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import med.voll.api.endereco.Endereco;
 
+@Entity(name = "Cliente")
+@Table(name = "clientes")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String telefone;
     private String email;
+    @Embedded
     private Endereco endereco;
+    private String cpf;
+    private boolean ativo;
 }
---
+++ src/main/java/med/voll/api/clientes/ClienteRepository.java
@@ -0,0 +1,6 @@
+package med.voll.api.clientes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente , Long>{
+}
